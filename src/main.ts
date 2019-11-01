fc6998617850f852e084346562277c75b2e0b281
Steven Hugg <hugg@fasterlight.com>
2019-10-31 19:44:51 -0500

    better query, i/o args

--- src/main.ts
+++ src/main.ts
@@ -9,46 +9,6 @@ function debug(...args) {
   if (verbose) process.stdout.write(util.format.apply(this, arguments) + '\n');
 }
 
-class StackMachine {
-    bus : Bus;
-    stack = [];
-    push(x:number) { this.stack.push(x|0); }
-    pushb(b:boolean) { this.push(b?1:0); }
-    pop():number { return this.stack.pop()|0; }
-    // unop    
-    u8()  { var x=this.pop(); this.push(x & 0xff); }
-    u16() { var x=this.pop(); this.push(x & 0xffff); }
-    sx8() { var x=this.pop(); this.push((x & 0x80) ? (x | ~0x7f) : (x & 0x7f)); }
-    sx16(){ var x=this.pop(); this.push((x & 0x8000) ? (x | ~0x7fff) : (x & 0x7fff)); }
-    neg() { this.push(-this.pop()); }
-    not() { this.push(~this.pop()); }
-    gz()  { this.pushb(this.pop() > 0); }
-    lz()  { this.pushb(this.pop() < 0); }
-    gez() { this.pushb(this.pop() >= 0); }
-    lez() { this.pushb(this.pop() <= 0); }
-    eqz() { this.pushb(this.pop() == 0); }
-    nez() { this.pushb(this.pop() != 0); }
-    // binop
-    add() { this.push(this.pop() + this.pop()); }
-    sub() { this.push(this.pop() - this.pop()); }
-    mul() { this.push(this.pop() * this.pop()); }
-    div() { this.push(this.pop() / this.pop()); }
-    mod() { this.push(this.pop() % this.pop()); }
-    and() { this.push(this.pop() & this.pop()); }
-    or()  { this.push(this.pop() | this.pop()); }
-    xor() { this.push(this.pop() ^ this.pop()); }
-    shl() { this.push(this.pop() << this.pop()); }
-    shr() { this.push(this.pop() >> this.pop()); }
-    lsr() { this.push(this.pop() >>> this.pop()); }
-    // triop
-    cond()    { var a=this.pop(); var b=this.pop(); var c=this.pop(); this.push(a?b:c); }
-    // memory
-    load8()   { return this.bus.read(this.pop()); }
-    store8()  { var a=this.pop(); var v=this.pop(); this.bus.write(a, v); }
-    load16()  { var a=this.pop(); var l=this.bus.read(a); var h=this.bus.read(a+1); return l+h*256; }
-    store16() { var a=this.pop(); var v=this.pop(); this.bus.write(a,v); this.bus.write(a+1,v>>8); }
-}
-
 const validinsns_6502 = [
   0, 2, 0, 0, 0, 2, 2, 0, 1, 2, 1, 0, 0, 3, 3, 0,
   2, 2, 0, 0, 0, 2, 2, 0, 1, 3, 0, 0, 0, 3, 3, 0,
@@ -91,6 +51,19 @@ class TestVector {
             this.mem[i] = israndom ? noise() : seed;
         }
     }
+    
+    getRegisters6502() {
+      return {
+        A: this.mem[0xff00],
+        X: this.mem[0xff01],
+        Y: this.mem[0xff02],
+        N: (this.mem[0xff03] & 0x80) ? 1 : 0,
+        V: (this.mem[0xff03] & 0x40) ? 1 : 0,
+        C: (this.mem[0xff03] & 0x01) ? 1 : 0,
+        Z: (this.mem[0xff03] & 0x02) ? 1 : 0,
+        SP: this.mem[0xff04],
+      }
+    };
 }
 
 const ZPSTART = 0x20;
@@ -144,14 +117,39 @@ class RunState implements Bus {
         else
             this.outputs[sym.sym] = value;
     }
-    
-    set(register: string, value: number) : void {
-        this.outputs[register] = value & 0xff;
+}
+
+class Inputs {
+    rs: RunState;
+    constructor(rs: RunState) { this.rs = rs; }
+
+    get(a: string|number|Array<string>) : number {
+        if (typeof a == 'string')
+            return this.getreg(a);
+        else if (typeof a == 'number')
+            return this.rs.read(a);
+        else if (a instanceof Array) {
+            let shift = 0;
+            let v = 0;
+            for (var b of a) {
+                v |= this.get(b) << shift;
+                shift += 8;
+            }
+            return v;
+        }
     }
-    
+    getreg(register: string) : number {
+        return this.rs.base.getRegisters6502()[register] & 0xff;
+    }
+    read(address: number) : number {
+        return this.rs.read(address);
+    }
+    read8(address: number) : number {
+        return this.rs.read(address);
+    }
     read16(address: number) : number {
-        var lo = this.read(address);
-        var hi = this.read(address+1);
+        var lo = this.rs.read(address);
+        var hi = this.rs.read(address+1);
         return lo + (hi<<8);
     }
     s8(x: number) : number {
@@ -159,6 +157,34 @@ class RunState implements Bus {
     }
     s16(x: number) : number {
         return (x & 0x8000) ? x-65536 : x;
+    }
+}
+
+class Outputs {
+    rs: RunState;
+    constructor(rs: RunState) { this.rs = rs; }
+    
+    set(a: string|number|Array<string>, v: number) : void {
+        if (typeof a == 'string')
+            this.rs.outputs[a] = v & 0xff;
+        else if (typeof a == 'number')
+            this.rs.write(a, v);
+        else if (a instanceof Array) {
+            for (var b of a) {
+                this.set(b, v & 0xff);
+                v >>= 8;
+            }
+        }
+    }
+    write(address: number, value : number) : void {
+        this.rs.write(address, value);
+    }
+    write8(address: number, value : number) : void {
+        this.rs.write(address, value);
+    }
+    write16(address: number, value : number) : void {
+        this.rs.write(address, value & 0xff);
+        this.rs.write(address+1, value >> 8);
     }
 }
 
@@ -174,15 +200,8 @@ export class TestRunner6502 {
         this.cpu.connectMemoryBus(this.rs);
         this.cpu.reset();
         var s0 = this.cpu.saveState();
-        s0.A = vec.mem[0xff00];
-        s0.X = vec.mem[0xff01];
-        s0.Y = vec.mem[0xff02];
-        s0.N = (vec.mem[0xff03] & 0x80) ? 1 : 0;
-        s0.V = (vec.mem[0xff03] & 0x40) ? 1 : 0;
-        s0.C = (vec.mem[0xff03] & 0x01) ? 1 : 0;
-        s0.Z = (vec.mem[0xff03] & 0x02) ? 1 : 0;
+        Object.assign(s0, vec.getRegisters6502());
         s0.D = 0;
-        s0.SP = noise();
         this.cpu.loadState(s0);
         var start = 0xff00;
         var end = start + insns.length - 1;
@@ -209,8 +228,7 @@ export class TestRunner6502 {
         for (var reg of ['A','X','Y','N','V','C','Z']) {
             r[reg] = s1[reg];
         }
-        //var endregs = [s1.A, s1.X, s1.Y, s1.N, s1.V, s1.C, s1.Z];
-        return r; //endregs.concat(this.rs.outputs);
+        return r;
     }
 
     validateSequence(insns: Uint8Array, start: number, maxlen: number) : number {
@@ -370,14 +388,20 @@ var maxseqlen = 32;
 
 var fingerprints = new Map();
 
+function allSameValues(arr) : boolean {
+    return arr.every((x) => x === arr[0]);
+}
+
 function getFingerprints(results) {
   var symbols = new Set<string>();
   results.forEach((vec) => { for (var k of Object.keys(vec)) symbols.add(k) });
   var prints = {};
   for (var sym of Array.from(symbols)) {
       var testv = results.map((vec) => vec[sym]);
-      testv = testv.map((v) => (v>=0) ? v.toString(16).padStart(2,'0') : 'x');
-      prints[sym] = testv.join('');
+      if (true) { // || !allSameValues(testv)) { // skip constant results?
+        testv = testv.map((v) => (v>=0) ? v.toString(16).padStart(2,'0') : 'x');
+        prints[sym] = testv.join('');
+      }
   }
   return prints;
 }
@@ -402,8 +426,8 @@ function scanFiles(db) {
   var fragid = 0;
   if (db) {
     console.log('#', db);
-    db.pragma('journal_mode = MEMORY'); // TODO?
-    db.pragma('synchronous = OFF'); // TODO?
+    db.pragma('journal_mode = MEMORY');
+    db.pragma('synchronous = OFF');
     var selectFragment = db.prepare("SELECT id FROM fragments WHERE insns = ?");
     var insertFragment = db.prepare("INSERT INTO fragments (insns) VALUES (?)");
     var insertSource = db.prepare("INSERT OR IGNORE INTO sources (fragid, filename, offset) VALUES (?,?,?)");
@@ -447,32 +471,37 @@ function scanFiles(db) {
 function runQuery(vec, func) {
     var rs = new RunState();
     rs.base = vec;
-    var rtn = func.call(rs, rs);
+    var rtn = func.call(rs, new Inputs(rs), new Outputs(rs));
     return rs.outputs;
 }
 
 function doQuery(db, funcbody:string) {
     var vecs = getTestVectors();
-    var func = new Function('$', '"use strict";' + funcbody);
+    var func = new Function('i', 'o', '"use strict";\n' + funcbody);
     var results = vecs.map((vec) => runQuery(vec, func));
-    //console.log(results);
     var prints = getFingerprints(results);
     console.log(prints);
     if (db) {
-        var qp = db.prepare("SELECT fragid FROM prints WHERE vec=? AND sym=?");
-        var qf = db.prepare("SELECT * FROM fragments f INNER JOIN sources s WHERE s.fragid=f.rowid AND s.fragid=?");
+        var args = [];
+        var sql = "SELECT DISTINCT insns,filename FROM fragments f INNER JOIN sources s WHERE s.fragid=f.rowid ";
         for (var sym in prints) {
-            var res = qp.all(prints[sym], sym);
-            for (var row of res) {
-                var frag = qf.get(row.fragid);
-                console.log(row.fragid,frag);
-            }
+            sql += " AND EXISTS (SELECT * FROM prints WHERE fragid=f.id AND sym=? AND vec=?)";
+            args.push(sym);
+            args.push(prints[sym]);
+        }
+        sql += " ORDER BY LENGTH(f.insns) DESC";
+        if (args.length == 0) {
+            console.log("No fingerprints generated.");
+        } else {
+            var q = db.prepare(sql);
+            var res = q.all(args);
+            for (var r of res) console.log(r);
         }
     }
 }
 
 verbose = options.verbose;
-if (verbose) console.log(options)
+debug(options);
 var db;
 if (options.db) {
   db = new sqlite3(options.db); //, { verbose: console.log });
